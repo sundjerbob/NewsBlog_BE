47bac20f270706783f49fec7c45fec29c75cf611
Tadija <tadijasimic999@gmail.com>
2023-10-28 22:31:39 +0200

    .

--- src/main/resources/migrations/structure_migration.sql
+++ src/main/resources/migrations/structure_migration.sql
@@ -1,18 +1,21 @@
-drop database if exists raf_news;
-create database raf_news;
+drop
+database if exists raf_news;
+create
+database raf_news;
 
-use raf_news;
+use
+raf_news;
 
 -- USERS --
 CREATE TABLE user
 (
-    id       INT AUTO_INCREMENT PRIMARY KEY,
-    email    VARCHAR(100) UNIQUE,
-    first_name     VARCHAR(100),
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    email      VARCHAR(100) UNIQUE,
+    first_name VARCHAR(100),
     last_name  VARCHAR(100),
-    password VARCHAR(255),
-    role     VARCHAR(50),
-    status   VARCHAR(50)
+    password   VARCHAR(255),
+    role       VARCHAR(50),
+    status     VARCHAR(50)
 );
 
 -- CATEGORIES --
@@ -42,7 +45,7 @@ CREATE TABLE comment
     id            INT AUTO_INCREMENT PRIMARY KEY,
     content       VARCHAR(500),
     creation_time TIMESTAMP,
-    author_id       INT,
+    author_id     INT,
     news_id       INT,
     FOREIGN KEY (author_id) REFERENCES user (id),
     FOREIGN KEY (news_id) REFERENCES news (id) ON DELETE CASCADE
